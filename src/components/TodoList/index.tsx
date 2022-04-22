1ec2cfb4823c2d30f32a37ef4bcbe8a071e53851
Guide Fari <guideg6@gmail.com>
2022-04-22 16:19:55 +0200

    useQuery example

--- src/components/TodoList/index.tsx
+++ src/components/TodoList/index.tsx
@@ -1,8 +1,19 @@
+import { useQuery, gql } from '@apollo/client'
+import { listTodos } from 'src/graphql/queries'
 import { mockData } from 'src/lib'
 import TodoItem from '../TodoItem'
 import * as Styled from './styles'
 
 const TodoList: React.FC = () => {
+  const { loading, error, data } = useQuery(
+    gql`
+      ${listTodos}
+    `
+  )
+  console.log(data)
+  console.log(error)
+  console.log(loading)
+
   return (
     <Styled.TodoList>
       {mockData.map((item) => (
